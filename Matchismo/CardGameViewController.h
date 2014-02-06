b908a083aa5965cfc36da2f5f58c917035954d3e
Roy Higgins <Roy@Roys-MacBook-Pro.local>
2014-02-05 21:06:52 -0600

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Roy Higgins on 2/5/14.
+//  Copyright (c) 2014 Roy Higgins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Roy Higgins on 2/5/14.
+//  Copyright (c) 2014 Roy Higgins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
